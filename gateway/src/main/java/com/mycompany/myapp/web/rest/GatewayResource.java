ba59193ddf810f3e3574e14586067f1228dadf62
Matt Raible <matt.raible@okta.com>
2020-02-18 22:23:03 -0700

    Enable actuator for gateway, hack routes data

--- gateway/src/main/java/com/mycompany/myapp/web/rest/GatewayResource.java
+++ gateway/src/main/java/com/mycompany/myapp/web/rest/GatewayResource.java
@@ -2,10 +2,9 @@ package com.mycompany.myapp.web.rest;
 
 import com.mycompany.myapp.security.AuthoritiesConstants;
 import com.mycompany.myapp.web.rest.vm.RouteVM;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.route.Route;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
@@ -28,6 +27,9 @@ public class GatewayResource {
 
     private final DiscoveryClient discoveryClient;
 
+    @Value("${spring.application.name}")
+    private String appName;
+
     public GatewayResource(RouteLocator routeLocator, DiscoveryClient discoveryClient) {
         this.routeLocator = routeLocator;
         this.discoveryClient = discoveryClient;
@@ -45,10 +47,17 @@ public class GatewayResource {
         List<RouteVM> routeVMs = new ArrayList<>();
         routes.subscribe(route -> {
             RouteVM routeVM = new RouteVM();
-            routeVM.setPath(route.getUri().getPath());
-            routeVM.setServiceId(route.getId());
-            routeVM.setServiceInstances(discoveryClient.getInstances(route.getId()));
-            routeVMs.add(routeVM);
+            // Manipulate strings to make Gateway routes look like Zuul's
+            String predicate = route.getPredicate().toString();
+            String path = predicate.substring(predicate.indexOf("[") + 1, predicate.indexOf("]"));
+            routeVM.setPath(path);
+            String serviceId = route.getId().substring(route.getId().indexOf("_") + 1).toLowerCase();
+            routeVM.setServiceId(serviceId);
+            // Exclude gateway app from routes
+            if (!serviceId.equalsIgnoreCase(appName)) {
+                routeVM.setServiceInstances(discoveryClient.getInstances(serviceId));
+                routeVMs.add(routeVM);
+            }
         });
         return ResponseEntity.ok(routeVMs);
     }
